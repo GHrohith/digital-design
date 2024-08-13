8d60f16f2d2b074f12498e88526323e3aecad538
GHrohith <rohithgeorgeninan@gmail.com>
2024-08-13 06:27:47 +0000

    LCD hex update

--- ide/lcd/codes/proj/src/my_comb.ino
+++ ide/lcd/codes/proj/src/my_comb.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12,11,5,4,3,2);//arduino pins connected to LCD
+const int contrastPin = 9;  
 
 int V_out_q=0;
 
@@ -12,6 +13,8 @@ void setup()
 lcd.begin(16,2);
 lcd.setCursor(0,0);
 lcd.print("----OHM METER---");
+pinMode(contrastPin, OUTPUT);
+analogWrite(contrastPin, 122);
 Serial.begin(9600);
 }
 
@@ -23,7 +26,7 @@ V_out_q=analogRead(0);//reading from A0
 //V_out is the actual voltage at the junction of R1 and R2
 V_out = V_in*V_out_q/1024;
 
-R2 = R1*((V_in)/(V_out)-1.0);
+R2 = R1/((V_in)/(V_out)-1.0);
 Serial.println(R2);
 lcd.setCursor(0,1);
 lcd.print("R = ");
